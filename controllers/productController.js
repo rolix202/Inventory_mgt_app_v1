79bd1835124350170e42e9f2d409e8a885dc87d0
rolix202 <williamsroland147@gmail.com>
2024-07-01 01:30:52 +0100

    configured multer for uploading images

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,37 @@
 import asyncHandler from "express-async-handler"
 import { body, validationResult } from "express-validator"
 import Category from "../models/category.js"
+import multer from "multer";
+import path from "path";
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads');
+    },
+    filename: (req, file, cb) => {
+        // const filename = file.originalname
+        // cb(null, filename)
+         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+         cb(null, uniqueSuffix + path.extname(file.originalname))
+    },
+})
+
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = /jpeg|jpg|png/;
+        const isMimeTypeValid = allowedTypes.test(file.mimetype);
+        const isExtNameValid = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+
+        if (isMimeTypeValid && isExtNameValid){
+            cb(null, true);
+        } else {
+            cb(new Error("Only image files are allowed!", false))
+        }
+    },
+    limits: { fileSize: 1024 * 1024 * 5 }
+})
 
 export const index = asyncHandler(async(req, res, next) => {
 
@@ -13,28 +44,40 @@ export const index = asyncHandler(async(req, res, next) => {
 })
 
 export const category_create_get = (req, res, next) => {
-    res.render("category_form", {title: "Create New Product Category"})
+    res.render("category_form", {title: "Create New Product Category", errors: []})
 }
 
 export const category_create_post = [
+    
+    upload.single('avatar'),
+
     body("category_name")
         .notEmpty()
-        .withMessage("Category name is required")
+        .withMessage("Category name is required!")
         .trim()
         .escape(),
-    body("category_desctiption")
+    body("category_description")
         .notEmpty()
-        .withMessage("Category description is required")
+        .withMessage("Category description is required!")
         .trim()
         .escape(),
 
-    asyncHandler((req, res, next) => {
+    asyncHandler(async(req, res, next) => {
         const errors = validationResult(req)
 
         const { category_name, category_description } = req.body
         // group them together in object
         // check for errors, if any, send it back to the form
         // send to database
+
+        if (!errors.isEmpty()){
+            return res.render("category_form", {
+                title: "Create New Product Category",
+                category_details,
+                errors: errors.array()
+            })
+        }
+
         
 })]
 
