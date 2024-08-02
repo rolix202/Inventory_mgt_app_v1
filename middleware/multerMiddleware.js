108be8653257586e14cfa344ccc4b629c67e50f8
rolix202 <williamsroland147@gmail.com>
2024-08-02 09:31:02 +0100

    added script for creating public folder for upload if not exists

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,9 +1,18 @@
 import multer from "multer";
 import path from "path"
+import fs from "fs"
+
+// Ensure the uploads directory exists
+const uploadsDir = path.join(__dirname, 'public/uploads')
+
+if(!fs.existsSync(uploadsDir)){
+    fs.mkdirSync(uploadsDir, {recursive: true})
+}
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/uploads');
+        cb(null, uploadsDir);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
